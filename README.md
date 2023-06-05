0b837d901cbbd9a5f84d8f0b323f89e2367371a2
Axel Hedman <axel.hedman@aalto.fi>
2023-06-05 18:48:51 +0300

    neural networks

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Machine learning with ml5.js
-I created this repository for me to try out different machine learning methods. Every program in this repository uses the ml5.js library:
+I created this repository for me to try out different machine learning methods using the ml5.js library:
 
 - ml5.js: https://ml5js.org/
+
+## Neural Network
+![Alt Text](gifs/data_collection.gif)
+![Alt Text](gifs/model_training.gif)
+![Alt Text](gifs/predictions.gif)
--- neural_network/sketch.js
+++ neural_network/sketch.js
@@ -1,4 +1,90 @@
+let model;
+let targetLabel = 'C';
+let program_state = 'collection';
+
 function setup() {
     createCanvas(500, 500);
-    background(0);
+
+    // Neural network configuration
+    let options = {
+        inputs: ['x', 'y'],
+        outputs: ['label'],
+        task: 'classification',
+        debug: 'true'
+    }
+
+    // Initializing the model 
+    model = ml5.neuralNetwork(options);
+    background(255);
 }
+
+function whileTraining(epoch, loss) {
+    console.log(epoch)
+}
+
+function finishedTraining() {
+    console.log('Finished training! Click anywhere on the JS canvas to generate a prediction.')
+    program_state = 'prediction';
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log(results);
+    stroke(0);
+    fill(0, 0, 255, 100);
+    ellipse(mouseX, mouseY, 24);
+    fill(0);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    text(results[0].label, mouseX, mouseY);
+}
+
+function keyPressed() {
+    if (program_state == 'collection') {
+        if (key == 'Enter') {
+            program_state = 'training';
+            console.log("Training model...")
+            model.normalizeData();
+            
+            let options = {
+                epochs: 200
+            }
+    
+            model.train(options, whileTraining, finishedTraining);
+            targetLabel = 'C'
+        }
+        else {
+            targetLabel = key.toUpperCase();
+        }
+    }
+}
+
+function mousePressed() {
+    let inputs = {
+        x: mouseX,
+        y: mouseY
+    }
+
+    if (program_state == 'collection') {
+        let target = {
+            label: targetLabel 
+        }
+        // Adding the training data to the model
+        model.addData(inputs, target)
+        
+        stroke(0);
+        noFill();
+        ellipse(mouseX, mouseY, 24);
+        fill(0);
+        noStroke();
+        textAlign(CENTER, CENTER);
+        text(targetLabel, mouseX, mouseY);
+    }
+    else if (program_state == 'prediction') {
+        console.log('Prediction:')
+        model.classify(inputs, gotResults);
+    }
+} 
